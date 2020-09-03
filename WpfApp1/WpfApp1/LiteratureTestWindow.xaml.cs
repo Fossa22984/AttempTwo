4086f8b879fe1afd6c1aefc4cf38dd191344ff12
Kuzmin00 <43570410+Kuzmin00@users.noreply.github.com>
2020-09-03 13:56:05 +0300

    Add Literature Test

---
+++ WpfApp1/WpfApp1/LiteratureTestWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for LiteratureTestWindow.xaml
+    /// </summary>
+    public partial class LiteratureTestWindow : Window
+    {
+        public LiteratureTestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnswereButton_Click(object sender, RoutedEventArgs e)
+        {
+            int i = 0;
+            if (AnswerRight1.IsChecked == true) i++;
+            if (AnswerRight2.IsChecked == true) i++;
+            if (AnswerRight3.IsChecked == true) i++;
+            if (AnswerRight4.IsChecked == true) i++;
+            if (AnswerRight5.IsChecked == true) i++;
+            if (AnswerRight6.IsChecked == true) i++;
+            MessageBox.Show($"Ваш балл: {i * 2} (2 балла за правильный ответ)");
+        }
+    }
+}
